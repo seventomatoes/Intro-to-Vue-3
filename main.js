ea825886da10e535d66a003d64ba7b8ef3e6f4b0
dmogire <dnm40@bath.ac.uk>
2026-09-17 16:04:49 +0100

    L5: created list

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [
+                {id: 1, size: 'XS'},
+                {id: 2, size: 'S'},
+                {id: 3, size: 'M'},
+                {id: 4, size: 'L'},
+                {id: 5, size: 'XL'},
+                {id: 6, size: 'XXL'},
+                {id: 7, size: 'XXXL'}
+            ]
         }
     }
 })
