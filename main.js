7f184fa145dc64580b55ee481da7b7c39011082c
dmogire <dnm40@bath.ac.uk>
2026-09-17 16:16:02 +0100

    L6: created remove from cart button

--- main.js
+++ main.js
@@ -11,5 +11,16 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            this.cart = Math.max(this.cart - 1, 0)
+        },
+        updateVariantImage(variantImage) {
+            this.image = variantImage
+        }
     }
 })
